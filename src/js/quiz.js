5fd43e962413b5a3211d6ac1e53329f3a264e54c
Udai Chauhan <udaichauhan284@gmail.com>
2021-02-02 22:18:14 +0530

    first commit

---
+++ src/js/quiz.js
@@ -0,0 +1,109 @@
+
+let questions = [
+    {
+      id: 1,
+      question: "Q1.What is the full form of RAM ?",
+      answer: "Random Access Memory",
+      options: [
+        "Random Access Memory",
+        "Randomely Access Memory",
+        "Run Aceapt Memory",
+        "None of these"
+      ]
+    },
+    {
+      id: 2,
+      question: "Q2.What is the full form of CPU?",
+      answer: "Central Processing Unit",
+      options: [
+        "Central Program Unit",
+        "Central Processing Unit",
+        "Central Preload Unit",
+        "None of these"
+      ]
+    },
+    {
+      id: 3,
+      question: "Q3.What is the full form of E-mail",
+      answer: "Electronic Mail",
+      options: [
+        "Electronic Mail",
+        "Electric Mail",
+        "Engine Mail",
+        "None of these"
+      ]
+    },
+    {
+        id:4,
+        question: "Q4.What is the full form of HTML?",
+        answer: "HyperText Markup Language",
+        options: [
+            "Hello To My Lord",
+            "Hey MakeUp Language",
+            "HyperTest Makeup Language",
+            "HyperText Markup Language"
+        ]
+    }
+  ];
+  
+  let question_count = 0;
+  let points = 0;
+  
+  window.onload = function() {
+    show(question_count);
+  
+  };
+  
+  function next() {
+  
+     
+    // if the question is last then redirect to final page
+    if (question_count == questions.length - 1) {
+      sessionStorage.setItem("time", `${minutes} minutes ${seconds} seconds`);
+      clearInterval(mytime);
+      location.href = "end.html";
+    }
+    console.log(question_count);
+  
+    let user_answer = document.querySelector("li.option.active").innerHTML;
+    // check if the answer is right or wrong
+    if (user_answer == questions[question_count].answer) {
+      points += 10;
+      sessionStorage.setItem("points", points);
+    }
+    console.log(points);
+  
+    question_count++;
+    show(question_count);
+  }
+  
+  function show(count) {
+    let question = document.getElementById("questions");
+    let [first, second, third, fourth] = questions[count].options;
+  
+    question.innerHTML = `
+    <h2>${questions[count].question}</h2>
+     <ul class="option_group">
+    <li class="option">${first}</li>
+    <li class="option">${second}</li>
+    <li class="option">${third}</li>
+    <li class="option">${fourth}</li>
+  </ul> 
+    `;
+    toggleActive();
+  }
+  
+  function toggleActive() {
+    let option = document.querySelectorAll("li.option");
+    for (let i = 0; i < option.length; i++) {
+      option[i].onclick = function() {
+        for (let i = 0; i < option.length; i++) {
+          if (option[i].classList.contains("active")) {
+            option[i].classList.remove("active");
+          }
+        }
+        option[i].classList.add("active");
+      };
+    }
+  }
+  
